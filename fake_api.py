e4ce90902fc084314da53baae75a8a55739f497c
Nicolas Abihaggle <rabihaggle@gmail.com>
2023-10-08 18:07:21 +0200

    add py

---
+++ fake_api.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    response = {
+        'message': 'Hello, World!'
+    }
+    return jsonify(response), 200
+
+@app.route('/database')
+def database_status():
+    response = {
+        'message': 'Database OK'
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
